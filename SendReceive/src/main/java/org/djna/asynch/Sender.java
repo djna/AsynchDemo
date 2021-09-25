c0365f1169882c00232e9471a52d36a17718bb9d
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-09-25 10:59:13 +0100

    Sample JMS sender and receiver

---
+++ SendReceive/src/main/java/org/djna/asynch/Sender.java
@@ -0,0 +1,43 @@
+package org.djna.asynch;
+
+import javax.jms.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+
+public  class Sender implements Runnable {
+
+    private Session session;
+    private String destination;
+    private int howManyToSend;
+
+    public Sender(Session initSession, String initDestination, int initHowManyToSend) {
+        this.session = initSession;
+        this.destination = initDestination;
+        howManyToSend = initHowManyToSend;
+    }
+
+    public Sender(Session initSession, String initDestination) {
+        this(initSession, initDestination, 10 );
+    }
+
+    public void run() {
+        try {
+            MessageProducer messageProducer = session.createProducer(session.createQueue(destination));
+            long counter = 0;
+
+            while (counter < howManyToSend) {
+                TextMessage message = session.createTextMessage("Message " + ++counter);
+                message.setJMSMessageID(UUID.randomUUID().toString());
+                messageProducer.send(message);
+                System.out.printf("Sent %d: %s%n", counter, message);
+            }
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
