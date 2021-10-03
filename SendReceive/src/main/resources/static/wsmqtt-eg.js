9b4460d7fad8bdc3c945213c46fb05841cf26318
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-10-03 09:13:42 +0100

    Skeleton web app

---
+++ SendReceive/src/main/resources/static/wsmqtt-eg.js
@@ -0,0 +1,34 @@
+client = new Paho.MQTT.Client("localhost", 61614, "BrowserClientId");
+
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+client.connect(
+    {onSuccess:onConnect, onFailure: onConnectionFailed}
+);
+
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("onConnect");
+  client.subscribe("home/thermostats");
+};
+
+
+function onConnectionFailed(responseObject) {
+  if (responseObject.errorCode !== 0)
+	console.log("onConnectionFailed:"+responseObject.errorMessage);
+};
+
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0)
+	console.log("onConnectionLost:"+responseObject.errorMessage);
+};
+
+function onMessageArrived(message) {
+   console.log("onMessageArrived:"+message.payloadString);
+   let telemetry = JSON.parse(message.payloadString);
+   console.log("Telemetry: " + telemetry.location
+               + ", " + telemetry.temperature
+               + ", " + new Date(telemetry.date)
+              );
+};
