b20e4b8e78e132559059e21e88f006fa8833ea73
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-10-04 04:20:40 +0100

    Web Socket example display

---
+++ SendReceive/src/main/java/org/djna/asynch/homeapp/HomeMonitorApplication.java
@@ -0,0 +1,14 @@
+package org.djna.asynch.homeapp;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeMonitorApplication.class, args);
+	}
+
+}
